5f8f45efab86a83e63ad783b8a3a04bb514dba8d
monlone <monlone@163.com>
2017-04-05 14:08:32 +0800

    init su

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+sudo systemctl restart nginx
+
+#if ! [ -L /webser/www/customer-service ]; then
+#  rm -rf /webser/www/customer-service
+#  ln -fs /vagrant /webser/www/customer-service
+#fi
